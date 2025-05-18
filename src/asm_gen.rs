d6a2167ea9d7bef70b0bb1bf06b7d88de1ae1891
Mvmo <mvmogo@gmail.com>
2025-05-19 00:33:15 +0200

    feat: adjust asm_gen to tacco

--- src/asm_gen.rs
+++ src/asm_gen.rs
@@ -1,4 +1,4 @@
-use crate::parser::{Expression, FunctionDef, Program, Statement};
+use crate::tacco_ir::{TaccoFunctionDef, TaccoInstruction, TaccoProgram, TaccoUnaryOperator, TaccoVal};
 
 #[derive(Debug)]
 pub struct AsmProgram {
@@ -13,40 +13,76 @@ pub struct AsmFunctionDef {
 
 #[derive(Debug)]
 pub enum AsmInstruction {
-    Mov(Operand, Operand),
+    Mov(AsmOperand, AsmOperand),
+    Unary(AsmUnaryOperator, AsmOperand),
     Ret
 }
 
 #[derive(Debug)]
-pub enum Operand {
-    Imm(i32),
-    Register
+pub enum Reg {
+    AX,
+    R10,
 }
 
-pub fn generate(program: Program) -> AsmProgram {
+#[derive(Debug)]
+pub enum AsmOperand {
+    Imm(i32),
+    Register(Reg),
+    Pseudo(String),
+    Stack(i32),
+}
+
+#[derive(Debug)]
+pub enum AsmUnaryOperator {
+    Neg,
+    Not
+}
+
+pub fn generate(program: TaccoProgram) -> AsmProgram {
     let function = translate_function_def(program.function_definition);
     return AsmProgram { function_definition: function }
 }
 
-fn translate_function_def(function_def: FunctionDef) -> AsmFunctionDef {
-    return AsmFunctionDef { name: function_def.name, instructions: translate_statement(function_def.body) }
+fn translate_function_def(function_def: TaccoFunctionDef) -> AsmFunctionDef {
+    let instructions: Vec<AsmInstruction> = function_def.body.iter()
+        .flat_map(translate_instruction)
+        .collect();
+
+    return AsmFunctionDef { name: function_def.identifier, instructions }
 }
 
-fn translate_statement(statement: Statement) -> Vec<AsmInstruction> {
-    match statement {
-        Statement::Return(expression) => {
-            return vec![
-                AsmInstruction::Mov(translate_expression(expression), Operand::Register),
-                AsmInstruction::Ret,
-            ]
-        }
+fn translate_instruction(instruction: &TaccoInstruction) -> Vec<AsmInstruction> {
+    return match instruction {
+        TaccoInstruction::Return(value) => vec![
+            AsmInstruction::Mov(
+                translate_operand(value),
+                AsmOperand::Register(Reg::AX)
+            ),
+            AsmInstruction::Ret
+        ],
+        TaccoInstruction::Unary { operator, src, dest } => vec![
+            AsmInstruction::Mov(
+                translate_operand(src),
+                translate_operand(dest),
+            ),
+            AsmInstruction::Unary(
+                translate_unary_operator(operator),
+                translate_operand(dest),
+            )
+        ]
     }
 }
 
-fn translate_expression(expression: Expression) -> Operand {
-    match expression {
-        Expression::Const(value) => {
-            Operand::Imm(value)
-        }
+fn translate_operand(value: &TaccoVal) -> AsmOperand {
+    return match value {
+        TaccoVal::Constant(int_value) => AsmOperand::Imm(*int_value),
+        TaccoVal::Var(identifier) => AsmOperand::Pseudo(identifier.clone())
+    }
+}
+
+fn translate_unary_operator(operator: &TaccoUnaryOperator) -> AsmUnaryOperator {
+    return match operator {
+        TaccoUnaryOperator::Complement => AsmUnaryOperator::Not,
+        TaccoUnaryOperator::Negate => AsmUnaryOperator::Neg,
     }
 }
