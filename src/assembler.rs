ac4c52fba70cc4dd098617c5e02887ececd5c047
Mvmo <mvmogo@gmail.com>
2025-05-18 03:40:22 +0200

    feat: implement basic final assembler, which just calls gcc

---
+++ src/assembler.rs
@@ -0,0 +1,18 @@
+use std::{path::PathBuf, process::{self, Command}};
+
+pub fn assemble(source: PathBuf) {
+    let command_result = Command::new("gcc")
+        .args(vec![
+            &source.to_string_lossy(),
+            "-o" ,
+            &source.with_extension("").to_string_lossy(),
+        ])
+        .spawn()
+        .expect("Assembler | Something wen't wrong spawning the process of the gcc assembler.")
+        .wait()
+        .expect("Assembler | Something wen't wrong executing the gcc assembler.");
+
+    if !command_result.success() {
+        process::exit(6);
+    }
+}
