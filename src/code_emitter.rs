aa3fe60c8a9771fc53a6028389ff7611cd3ce972
Mvmo <mvmogo@gmail.com>
2025-05-19 02:56:19 +0200

    fix: emit working assembly :)

--- src/code_emitter.rs
+++ src/code_emitter.rs
@@ -1,6 +1,6 @@
-use std::{fs, path::PathBuf, process};
+use std::{fs, ops::Neg, path::PathBuf, process};
 
-use crate::{asm_gen::{AsmFunctionDef, AsmInstruction, AsmProgram, AsmOperand}, parser::Program};
+use crate::asm_gen::{AsmFunctionDef, AsmInstruction, AsmOperand, AsmProgram, AsmUnaryOperator, Reg};
 
 pub fn emit(asm_program: AsmProgram, to: PathBuf) {
     let mut lines = Vec::new();
@@ -16,8 +16,13 @@ fn write_program(lines: &mut Vec<String>, program: AsmProgram) {
 }
 
 fn write_function_def(lines: &mut Vec<String>, function_def: AsmFunctionDef) {
-    lines.push(format!(".globl _{}", function_def.name));
-    lines.push(format!("_{}:", function_def.name));
+    lines.push(format!("
+            .globl _{0}
+        _{0}:
+            pushq %rbp
+            movq %rsp, %rbp
+    ", function_def.name));
+
     for instruction in function_def.instructions {
         write_instruction(lines, instruction);
     }
@@ -25,20 +30,32 @@ fn write_function_def(lines: &mut Vec<String>, function_def: AsmFunctionDef) {
 
 fn write_instruction(lines: &mut Vec<String>, instruction: AsmInstruction) {
     match instruction {
-        AsmInstruction::Mov(src, dest) => {
-            lines.push(format!("movl {}, {}", translate_operand(src), translate_operand(dest)));
-        },
-        AsmInstruction::Ret => {
-            lines.push(format!("ret"));
-        },
-        _ => {}
+        AsmInstruction::Mov(src, dest) => lines.push(format!("
+            movl {}, {}
+        ", translate_operand(src), translate_operand(dest))),
+        AsmInstruction::Ret => lines.push(format!("
+            movq %rbp, %rsp
+            popq %rbp
+            ret
+        ")),
+        AsmInstruction::AllocateStack(size) => lines.push(format!("
+            subq ${}, %rsp
+        ", size)),
+        AsmInstruction::Unary(AsmUnaryOperator::Neg, operand) => lines.push(format!("
+            negl {}
+        ", translate_operand(operand))),
+        AsmInstruction::Unary(AsmUnaryOperator::Not, operand) => lines.push(format!("
+            notl {}
+        ", translate_operand(operand))),
     }
 }
 
 fn translate_operand(operand: AsmOperand) -> String {
     match operand {
+        AsmOperand::Register(Reg::AX) => format!("%eax"),
+        AsmOperand::Register(Reg::R10) => format!("%r10d"),
+        AsmOperand::Stack(offset) => format!("{}(%rbp)", offset),
         AsmOperand::Imm(int_value) => format!("${}", int_value),
-        AsmOperand::Register(_) => format!("%eax"),
-        _ => format!("")
+        AsmOperand::Pseudo(_) => process::exit(7)
     }
 }
