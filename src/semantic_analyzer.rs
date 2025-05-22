c4adbd8b7f7398932a14d9dd5b8d0feb9bf13b24
Mvmo <mvmogo@gmail.com>
2025-05-22 16:49:54 +0200

    feat: semantic analysis for if and conditionals

--- src/semantic_analyzer.rs
+++ src/semantic_analyzer.rs
@@ -7,7 +7,7 @@ pub fn validate(program: Program) -> Program {
 
     let body = program.function_definition.body.iter().map(|block_item| {
         match block_item {
-            BlockItem::Statement(statement) => BlockItem::Statement(resolve_statement(statement.clone(), &mut variable_map)),
+            BlockItem::Statement(statement) => BlockItem::Statement(resolve_statement(statement, &mut variable_map)),
             BlockItem::Declaration(declaration) => BlockItem::Declaration(resolve_declaration(declaration.clone(), &mut variable_map)),
         }
     }).collect();
@@ -31,29 +31,33 @@ fn resolve_declaration(declaration: Declaration, variable_map: &mut HashMap<Stri
 
     let mut init = None;
     if let Some(init_expr) = declaration.initializer {
-        init = Some(resolve_expression(init_expr, variable_map));
+        init = Some(resolve_expression(&init_expr, variable_map));
     }
 
     return Declaration{ name: unique_name, initializer: init };
 }
 
-fn resolve_statement(statement: Statement, variable_map: &mut HashMap<String, String>) -> Statement {
+fn resolve_statement(statement: &Statement, variable_map: &mut HashMap<String, String>) -> Statement {
     return match statement {
-        Statement::Return(expression) => Statement::Return(resolve_expression(expression, variable_map)),
-        Statement::Expression(expression) => Statement::Expression(resolve_expression(expression, variable_map)),
+        Statement::Return(expression) => Statement::Return(resolve_expression(&expression, variable_map)),
+        Statement::Expression(expression) => Statement::Expression(resolve_expression(&expression, variable_map)),
         Statement::Null => Statement::Null,
-        _ => todo!()
+        Statement::If { condition, then, _else } => Statement::If {
+            condition: resolve_expression(&condition, variable_map),
+            then: Box::new(resolve_statement(then.as_ref(), variable_map)),
+            _else: _else.as_ref().map(|statement| Box::new(resolve_statement(&statement, variable_map)))
+        }
     }
 }
 
-fn resolve_expression(expression: Expression, variable_map: &mut HashMap<String, String>) -> Expression {
+fn resolve_expression(expression: &Expression, variable_map: &mut HashMap<String, String>) -> Expression {
     match expression {
         Expression::Assignment(assignment_operator, left, right) => {
             if let Expression::Var(_) = left.as_ref() {
                 return Expression::Assignment(
-                    assignment_operator,
-                    Box::new(resolve_expression(left.as_ref().clone(), variable_map)),
-                    Box::new(resolve_expression(right.as_ref().clone(), variable_map)),
+                    assignment_operator.clone(),
+                    Box::new(resolve_expression(left, variable_map)),
+                    Box::new(resolve_expression(right, variable_map)),
                 )
             }
 
@@ -61,24 +65,28 @@ fn resolve_expression(expression: Expression, variable_map: &mut HashMap<String,
             process::exit(8)
         },
         Expression::Var(identifier) => {
-            if variable_map.contains_key(&identifier) {
-                return Expression::Var(variable_map.get(&identifier).unwrap().to_string())
+            if variable_map.contains_key(identifier) {
+                return Expression::Var(variable_map.get(identifier).unwrap().to_string())
             }
 
             println!("Semantic Analyzer | Undeclared variable use");
             process::exit(8)
         },
         Expression::Binary { operator, left, right } => Expression::Binary {
-            operator,
-            left: Box::new(resolve_expression(left.as_ref().clone(), variable_map)),
-            right: Box::new(resolve_expression(right.as_ref().clone(), variable_map)),
+            operator: operator.clone(),
+            left: Box::new(resolve_expression(left, variable_map)),
+            right: Box::new(resolve_expression(right, variable_map)),
         },
         Expression::Unary { operator, inner_expression } => Expression::Unary {
-            operator,
-            inner_expression: Box::new(resolve_expression(inner_expression.as_ref().clone(), variable_map)),
+            operator: operator.clone(),
+            inner_expression: Box::new(resolve_expression(inner_expression, variable_map)),
         },
-        Expression::Const(int_value) => Expression::Const(int_value),
-        _ => todo!()
+        Expression::Const(int_value) => Expression::Const(*int_value),
+        Expression::Conditional { condition, if_true, _else } => Expression::Conditional {
+            condition: Box::new(resolve_expression(condition, variable_map)),
+            if_true: Box::new(resolve_expression(if_true, variable_map)),
+            _else: Box::new(resolve_expression(_else, variable_map)),
+        }
     }
 }
 
