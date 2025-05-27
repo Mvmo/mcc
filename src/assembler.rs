b2d5b90f4cac039944e650ca8117035276621b23
Mvmo <mvmogo@gmail.com>
2025-05-27 07:16:13 +0200

    feat: rename with_gcc to perform executable assembly and add perform_object_assembly method

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,6 +1,24 @@
 use std::{path::PathBuf, process::{self, Command}};
 
-pub fn with_gcc(source: PathBuf) {
+pub fn perform_object_assembly(source: PathBuf) {
+    let command_result = Command::new("gcc")
+        .args(vec![
+            "-c",
+            &source.to_string_lossy(),
+            "-o" ,
+            &source.with_extension("o").to_string_lossy(),
+        ])
+        .spawn()
+        .expect("Assembler | Something wen't wrong spawning the process of the gcc assembler.")
+        .wait()
+        .expect("Assembler | Something wen't wrong executing the gcc assembler.");
+
+    if !command_result.success() {
+        process::exit(6);
+    }
+}
+
+pub fn perform_executable_assembly(source: PathBuf) {
     let command_result = Command::new("gcc")
         .args(vec![
             &source.to_string_lossy(),
